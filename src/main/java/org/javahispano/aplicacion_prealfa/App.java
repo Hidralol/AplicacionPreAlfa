ad89ea8d409e16c57c41688854fff36257ca9263
Hidra <artic4_6@hotmail.com>
2018-04-22 12:58:35 +0200

    Commit inicial

---
+++ src/main/java/org/javahispano/aplicacion_prealfa/App.java
@@ -0,0 +1,13 @@
+package org.javahispano.aplicacion_prealfa;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
